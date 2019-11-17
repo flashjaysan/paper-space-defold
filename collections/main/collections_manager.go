021b9c15207d9cbd76302597902b5a9e9f4bdffe
flashjaysan <jaysan@wanadoo.fr>
2019-11-17 19:40:06 +0100

    Minor fixes
    Fixed ship bugs.
    Cleaned up scripts.

---
+++ collections/main/collections_manager.go
@@ -0,0 +1,69 @@
+components {
+  id: "main"
+  component: "/collections/main/collections_manager.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "title_proxy"
+  type: "collectionproxy"
+  data: "collection: \"/collections/game.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "game_proxy"
+  type: "collectionproxy"
+  data: "collection: \"/collections/game.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "gameover_proxy"
+  type: "collectionproxy"
+  data: "collection: \"/collections/gameover.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
